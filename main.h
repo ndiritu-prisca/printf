e524a9ce54e188ebc5b06b67deb5ec4a5ee44b22
Mussbront <vmogendi@gmail.com>
2022-11-24 03:25:57 -0800

    Header file

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+
+/**
+  * struct printf_control- This is a typedef struct
+  * @i: Pointer type char.
+  * @f: Pointer to call the function.
+  */
+
+typedef struct printf_control
+{
+	char *i;
+	void (*f)(va_list);
+} _printf_t;
+int get_print(const char *format, _printf_t gets[], va_list ap);
+int print_int(va_list ap);
+int _putchar(char ch);
+int _puts(char *str);
+int print_dec(va_list ap);
+int print_str(va_list ap);
+int print_char(va_list ap);
+int print_percent(va_list ap);
+int count_digit(int i);
+int _abs(int i);
+int integer(int i);
+#endif
