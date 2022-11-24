5f5a5f50aafdeb5c68653e013f288f9fdb8af31a
Mussbront <vmogendi@gmail.com>
2022-11-24 15:50:32 -0800

    comments

--- main.h
+++ main.h
@@ -19,16 +19,14 @@ typedef struct printf_control
 	int (*f)(va_list);
 } _printf_t;
 int get_print(const char *format, _printf_t gets[], va_list ap);
-/*int print_int(va_list ap);*/
+int print_int(va_list ap);
 int _putchar(char ch);
 int _puts(char *str);
 int print_dec(va_list ap);
 int print_str(va_list ap);
 int print_char(va_list ap);
 int print_percent(va_list ap);
-/**
-  * int count_digit(int i);
-  * int _abs(int i);
-  * int integer(int i);
-  */
+int count_digit(int i);
+int _abs(int i);
+int integer(int i);
 #endif
