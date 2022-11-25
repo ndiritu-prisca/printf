2ca8e3e5979bfd2885c67adb498a6c5cba681321
Mussbront <vmogendi@gmail.com>
2022-11-25 06:09:39 -0800

    Octal

---
+++ print_octal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * print_octal - function that converts to octal base
+  * @ap: va_list argument passed
+  * Return: count
+  */
+
+int print_octal(va_list ap)
+{
+	int count = 0, i;
+	int *octa;
+	unsigned int n = va_arg(ap, unsigned int);
+	unsigned int num = n;
+
+	while (n / 8 != 0)
+	{
+		n /= 8;
+		count++;
+	}
+	count++;
+	octa = malloc(count * sizeof(int));
+	for (i = 0; i < count; i++)
+	{
+		octa[i] = num % 8;
+		num /= 8;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(octa[i] + '0');
+	}
+	free(octa);
+	return (count);
+}
