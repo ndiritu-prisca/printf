8bf819a2447938d0344e03b533d672008bacbe6e
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-25 06:07:32 -0800

    Print_binary

---
+++ print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+  * print_binary - a function that converts unsigned int to binary
+  * @ap: list of arguments
+  * Return: number of characters
+  */
+int print_binary(va_list ap)
+{
+	int numc = 0, x;
+	int *p;
+	unsigned int i = va_arg(ap, unsigned int);
+	unsigned int tmp = i;
+
+	while (i / 2 != 0)
+	{
+		i /= 2;
+		numc++;
+	}
+	numc++;
+	p = malloc(numc * sizeof(*p));
+	if (p == NULL)
+	{
+		free(p);
+		return (0);
+	}
+	for (x = 0; x < numc; x++)
+	{
+		p[x] = tmp % 2;
+		tmp /= 2;
+	}
+	for (x = numc - 1; x >= 0; x--)
+	{
+		_putchar(p[x] + '0');
+	}
+	free(p);
+	return (numc);
+}
