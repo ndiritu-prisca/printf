d78a53ceb1039394d0188c6518db216ef40f1a1a
ndiritu-prisca <priscandiritu@outlook.com>
2022-11-29 04:51:54 -0800

    str

--- print_str.c
+++ print_str.c
@@ -3,17 +3,24 @@
 /**
   * print_str - Prints string
   * @ap: va_list arg
+  * @buf: buffer size
   * Return: str count
   */
 
-int print_str(va_list ap)
+int print_str(va_list ap, char *buf)
 {
-	char *str = va_arg(ap, char *);
+	char *str;
 	int count = 0;
 	int i;
+	
+	buf = va_arg(ap, char *);
 
-	if (str == NULL)
+	if (!buf)
+	{
 		str = "(null)";
+		return (write(1, str, 6));
+	}
+	str = buf;
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
